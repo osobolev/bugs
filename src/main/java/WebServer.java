815c7da62c45970f95bd94ae80303275ce4f030d
oleg <sobolev_ov@mail.ru>
2016-06-02 19:30:04 +0300

    Версия Tomcat

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,27 +1,17 @@
-import allbugs.AllBugsServlet;
-import bug.BugServlet;
-import bugs.BugsServlet;
-import common.MainServlet;
-import login.LoginServlet;
-import login.LogoutServlet;
-import newbug.NewBugServlet;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-
 public class WebServer {
 
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        server.setHandler(ctx);
-        ctx.addServlet(MainServlet.class, "/");
-        ctx.addServlet(BugsServlet.class, "/bugs");
-        ctx.addServlet(LoginServlet.class, "/login");
-        ctx.addServlet(LogoutServlet.class, "/logout");
-        ctx.addServlet(AllBugsServlet.class, "/allbugs");
-        ctx.addServlet(BugServlet.class, "/bug");
-        ctx.addServlet(NewBugServlet.class, "/newbug");
-        ctx.addServlet(StaticServlet.class, "/static/*");
-        server.start();
-    }
+//    public static void main(String[] args) throws Exception {
+//        Server server = new Server(8080);
+//        ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.SESSIONS);
+//        server.setHandler(ctx);
+//        ctx.addServlet(MainServlet.class, "/");
+//        ctx.addServlet(BugsServlet.class, "/bugs");
+//        ctx.addServlet(LoginServlet.class, "/login");
+//        ctx.addServlet(LogoutServlet.class, "/logout");
+//        ctx.addServlet(AllBugsServlet.class, "/allbugs");
+//        ctx.addServlet(BugServlet.class, "/bug");
+//        ctx.addServlet(NewBugServlet.class, "/newbug");
+//        ctx.addServlet(StaticServlet.class, "/static/*");
+//        server.start();
+//    }
 }
--- src/main/java/common/DB.java
+++ src/main/java/common/DB.java
@@ -4,4 +4,12 @@ public class DB {
 
 //    public static final String DB_NAME = "jdbc:h2:file:/F:/bugs";
     public static final String DB_NAME = "jdbc:h2:~/bugs";
+
+    static {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
